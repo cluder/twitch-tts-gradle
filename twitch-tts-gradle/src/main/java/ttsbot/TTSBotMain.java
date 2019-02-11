141e6c626e8a15651bb676f0f97c458d64f0f558
cluder <christoph.luder@gmail.com>
2019-02-11 11:39:42 +0100

    added java 8 / 64 bit check

--- twitch-tts-gradle/src/main/java/ttsbot/TTSBotMain.java
+++ twitch-tts-gradle/src/main/java/ttsbot/TTSBotMain.java
@@ -1,5 +1,9 @@
 package ttsbot;
 
+import javax.swing.JOptionPane;
+
+import org.apache.commons.lang3.JavaVersion;
+import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,7 +34,28 @@ public class TTSBotMain {
 	}
 
 	public static void main(String[] args) {
+		startupCheck();
+
 		TTSBotMain bot = new TTSBotMain();
 		bot.start();
+	}
+
+	/**
+	 * Check if we have java 8+ 64bit.
+	 */
+	private static void startupCheck() {
+		final String os_arch = System.getProperty("os.arch");
+		final String java_version = System.getProperty("java.specification.version");
+		log.info("os.arch:" + os_arch + " version:" + java_version);
+
+		final boolean javaVersionAtLeast8 = SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_1_8);
+		log.info("java version 8+: " + javaVersionAtLeast8);
+
+		if (os_arch.contains("64") == false && javaVersionAtLeast8) {
+			JOptionPane.showMessageDialog(null, "A 64-bit Java 8 or higher installation is needed",
+					"64 Bit Java needed", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+
 	}
 }
